d54c262dbf18e9dbde847610744c9362ef9affa6
E.Azer Koçulu <azer@kodfabrik.com>
2013-07-21 15:18:48 -0700

    pop

--- content.js
+++ content.js
@@ -3,6 +3,7 @@ module.exports = {
   glass: 'http://i.cloudup.com/E021I9zUG3.m4a',
   funk: 'http://i.cloudup.com/KkfWRzYC77.m4a',
   morse: 'http://i.cloudup.com/h7r7MsF4q3.m4a',
+  pop: 'http://i.cloudup.com/vTka9yOizT.m4a',
   purr: 'http://i.cloudup.com/5HJSHCtOzZ.m4a',
   tink: 'http://i.cloudup.com/nCtoNq3kJN.m4a',
   submarine: 'http://i.cloudup.com/r4ZENSF0Hu.m4a'
